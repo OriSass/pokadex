81d23cad47bf78ee72138c4836bfc366f99f415f
OriSass <67857592+OriSass@users.noreply.github.com>
2020-08-06 13:57:31 +0300

    finisheddd

--- main.js
+++ main.js
@@ -14,25 +14,29 @@ let sButton = document.getElementById("searchButton");
 sButton.addEventListener("click", searchPokemon);
 
 function showPokeStats(data) {
-  console.log(data);
+  //console.log(data);
   let name = data["name"];
   let height = data["height"];
   let weight = data["weight"];
   let type = data["type"];
   let frontImg = data.sprites.front_default;
   let backImg = data.sprites.back_default;
-  //res is the results div
+  
+  // res is the results div
   let res = document.getElementById("results");
   let typesHtml = makeTypes(data);
+  //addButtonEvents();
   makeDiv(name, height, weight, frontImg, typesHtml, res);
+  
+  // setting front & back image!
   let img = document.getElementById("pokeImg");
   img.addEventListener("mouseover", () => {
-    //let currentUrl = img.getAttribute("src");
     img.setAttribute("src", backImg);
   });
   img.addEventListener("mouseout", () => {
     img.setAttribute("src", frontImg);
   })
+  addButtonEvents();
 }
 const makeDiv = (name, height, weight, frontImg, typesHtml,  container) => {
   const htmlText = `
@@ -46,11 +50,60 @@ const makeDiv = (name, height, weight, frontImg, typesHtml,  container) => {
         `;
   container.innerHTML = htmlText;
 }
+
+
 const makeTypes = data => {
   let arr = data.types;
   let html = "";
   for (let index = 0; index < arr.length; index++) {
-    html += `<div>Type ${index + 1}: ${arr[index].type.name}</div>`;
+    let pokeType = arr[index].type.name;
+    html += `<div>Type ${index + 1}: ${pokeType}</div><button id='${pokeType}' onclick=showPokemonByType(${pokeType})>More ${pokeType} Pokemon</button>`;
   } 
   return html;
+}
+
+function addButtonEvents(){
+    let buttonArr = document.getElementsByTagName("button");
+    if(buttonArr.length > 1)
+    {
+      for(let i = 1; i < buttonArr.length; i++)
+      {
+        buttonArr[i].addEventListener('click', showPokemonByType);
+      }
+    }
+}
+const showPokemonByType = async () =>{
+   let id = event.target.id;
+   const { data } = await axios.get(`https://pokeapi.co/api/v2/type/${id}`);
+   console.log(data);
+   let pokemons = data["pokemon"];
+   let results = document.getElementById("results");
+   //let html = results.innerHTML;
+   let newContent = `<div id="pokemonFromType">`;
+   for (let i = 0; i < pokemons.length; i++) {
+    newContent += `<div id="${i}">${pokemons[i].pokemon.name}</div>`;
+   }
+   newContent += `</div>`;
+   let oldContent = document.getElementById("pokemonFromType");
+   if(oldContent === null){
+     results.innerHTML += newContent;
+   }
+   else{
+     oldContent.remove();
+     let results = document.getElementById("results");
+     results.innerHTML += newContent;
+   }
+   let container = document.getElementById("pokemonFromType").childNodes;
+   container.forEach((pokemon) => {
+    pokemon.addEventListener("click", onClickPokemon);
+   })
+
+}
+
+function onClickPokemon(){
+  let pokemonName = event.target.innerHTML;
+  document.getElementById("search").value = pokemonName;
+  let res = document.getElementById("results");
+  res.innerHTML = "";
+  searchPokemon(pokemonName);
 }
